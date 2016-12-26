ac760705088013e2d9d47456b0650f627395b09f
Nils-Boerge Margotti <nilsmargotti@gmail.com>
2016-12-26 14:42:58 +0100

    Add test

---
+++ test/basic.js
@@ -0,0 +1,14 @@
+/**
+ * Basic test of TeamspeakClientQuery
+ * @author Nils-Boerge Margotti <margotni@kalmiya.de>
+ */
+
+let test = require('unit.js');
+
+let TeamspeakClientQuery = require('../src/TeamspeakClientQuery.js');
+
+describe("TeamspeakClientQuery", function() {
+  it('load', function() {
+    return true;
+  });
+});
